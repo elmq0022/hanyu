bf33ac3185fe2043984fc9393eb9c49f8df36af9
zero_loss <elmq0022@umn.edu>
2017-03-30 22:50:27 -0500

    created a form, view, and template for the search

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+'''
+Views for the dictionary application.
+'''
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import SearchForm
+
+def search(request):
+    '''
+    This is a first crack at a basic search interface.
+    '''
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            # TODO: Make this do someting.
+            return HttpResponseRedirect('some where')
+    else:
+        form = SearchForm()
+
+    return render(request, 'dictionary/search.html', {'form': form})
