22ad3a4d7e222504f560f5e5547caec2ce6ce46a
zero_loss <elmq0022@umn.edu>
2017-04-08 22:00:53 -0500

    changed Entry model to contain all  CC-CEDICT information

--- dictionary/management/commands/load_cedict.py
+++ dictionary/management/commands/load_cedict.py
@@ -1,7 +1,13 @@
+'''
+This module will load the Entry table using the CC-CEDIct Chinese to English dictionary located
+at https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip
+'''
+
 import io
 import re
-import requests
 import zipfile
+
+import requests
 from django.core.management.base import BaseCommand, CommandError
 
 from dictionary import models
@@ -25,70 +31,52 @@ class Command(BaseCommand):
         (?P<pin_yin>[a-z\d\s]+)             # pronunc pattern
         \]                                  # end pronunc
         \s+                                 # spaces
-        /                                   # start the english equivalents
-        (?P<english_equivalents>.+)         # definitions
-        /                                   # end the english equivalents
+        /                                   # start the defintions 
+        (?P<definitions>.+)                 # definitions
+        /                                   # end the defintions 
         '''
         self.valid_entries = re.compile(pattern, re.M|re.I|re.X)
         self.url = r'https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip'
 
     def download_dict(self):
         '''
-        Download dictionary zip file from ??? and return the contents as a string
+        Download Chinese-English dictionary zip file and return the contents as a string.
         '''
         r = requests.get(self.url)
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall()
         file_name = z.namelist()[0]
-        f = z.open(file_name)
-        text = f.read().decode("utf-8")
+        with z.open(file_name) as f:
+            text = f.read().decode("utf-8")
         return text
-
-    def add_arguments(self, parser):
-        pass
-
-    def delete_entries_from_db(self):
-        models.Definition.objects.all().delete()
-        models.Entry.objects.all().delete()
 
     def add_to_db(self, matches):
         count = 0
         entries = []
-        definitions = []
 
-        for i, match in enumerate(matches):
+        for match in matches:
             entry = models.Entry(
                 traditional=match['traditional'],
                 simple=match['simple'],
-                pronunciation=match['pin_yin'],
-                order=i
+                pin_yin=match['pin_yin'],
+                definitions=match['definitions']
             )
             entries.append(entry)
             count += 1
 
         models.Entry.objects.bulk_create(entries)
-
-        for i, match in enumerate(matches):
-            for j, item in enumerate(match['english_equivalents'].strip().split('/')):
-                definition = models.Definition(
-                    entry_id=i,
-                    order=j,
-                    text=item
-                )
-                definitions.append(definition)
-
-        models.Definition.objects.bulk_create(definitions)
-        print("There were {} entries inserted into the database".format(count))
+        return count
 
     def handle(self, *args, **options):
-        print("Downloading dictionary from {}".format(self.url))
+        print("Downloading dictionary from {}.".format(self.url))
         text = self.download_dict()
-        print("Download complete")
+        print("Download complete.")
 
-        print("Deleting entries from database")
-        self.delete_entries_from_db()        
-        
-        print("Entering data into database")
-        matches = list(self.valid_entries.finditer(text))
-        self.add_to_db(matches)
-        print('done')
+        print("Deleting entries from database.")
+        models.Entry.objects.all().delete()
+
+        print("Entering data into database.")
+        matches = self.valid_entries.finditer(text)
+        count = self.add_to_db(matches)
+
+        print("Complete. There were {} entries entered into the database.".format(count))
