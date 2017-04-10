b4c9d29e21b48c1cbc2623aca66558892438d507
zero_loss <elmq0022@umn.edu>
2017-04-09 23:01:32 -0500

    adding accounts

---
+++ hanyu/views.py
@@ -0,0 +1,11 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(reverse('dictionary:search'))
+        return super().get(request, *args, **kwargs)
