a61f6784445af5c5b0946791320d36eee516e6c0
zero_loss <elmq0022@umn.edu>
2017-04-16 14:42:39 -0500

    first pass at extracting full text from page

--- analysis/management/commands/single_character_analysis.py
+++ analysis/management/commands/single_character_analysis.py
@@ -1,4 +1,8 @@
+import os
+from pprint import pprint
+
 from django.core.management.base import BaseCommand, CommandError
+from lxml import html
 
 
 class Command(BaseCommand):
@@ -8,10 +12,15 @@ class Command(BaseCommand):
     '''
 
     def __init__(self):
+        self.base_dir = os.path.dirname(os.path.abspath(__file__))
         super().__init__()
 
     def handle(self, *args, **options):
-        with open(r'./resources/wiki_zh_china.xml') as f:
-            article = f.readline()
-            print(article[:10])
-
+        with open(os.path.join(self.base_dir, 'resources', 'wiki_zh_china.xml'), encoding="utf-8") as f:
+            article = f.read()
+            tree = html.fromstring(article)
+            # content = tree.xpath(r'//*[@id="mw-content-text"]/*/text()')
+            # content = tree.xpath(r'//*[@id="mw-content-text"]/p/text()|//*[@id="mw-content-text"]/p/*/text()')
+            # content = tree.xpath(r'//*[@id="mw-content-text"]/p/descendant-or-self')
+            content = tree.xpath(r'//*[@id="mw-content-text"]//text()')
+            pprint(content[500:600])
