0507a4bcf0960acbe89af44e885e5559b2348bf1
zero_loss <elmq0022@umn.edu>
2017-05-05 23:00:59 -0500

    More busting up AdminLTE in to files

--- hanyu/views.py
+++ hanyu/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
-    template_name = 'index.html'
+    template_name = 'test.html'
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
