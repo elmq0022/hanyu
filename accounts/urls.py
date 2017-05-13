343fc2d95f8ef64bfb1e7908582514ae6e984da9
zero_loss <elmq0022@umn.edu>
2017-05-12 21:38:14 -0500

    making urls a little easier

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns =[
     url(r'login/$', views.LoginView.as_view(), name='login'),
     url(r'logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'signup/$', views.SignUpView.as_view(), name='signup'),
-    url(r'home/$', views.Home.as_view(), name='home'),
+    url(r'register/$', views.SignUpView.as_view(), name='register'),
+    url(r'profile/$', views.Home.as_view(), name='profile'),
 ]
--- hanyu/templatetags/AdminLTE.py
+++ hanyu/templatetags/AdminLTE.py
@@ -1,12 +1,23 @@
-from pdb import set_trace
 from django import template 
-from django.utils.html import format_html, format_html_join
-from django.conf import settings
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
+
 @register.inclusion_tag('_menu_tree.html', takes_context=True)
-def menu_tree(context):
-    context['menu'] = settings.SITE_MENU
-    print(context)
+def menu_tree(context, heading, *args):
+    '''
+    This is a tag used to build the AdminLTE 2 tree menu
+    The first argument is the menu heading.
+    The following arguments are alternating namespace:urls and the description
+    '''
+
+    links = []
+    from pdb import set_trace
+    set_trace()
+    for url, desc in zip(args[0::2], args[1::2]):
+        links.append({'url':reverse(url), 'desc':desc})
+
+    context['heading'] = heading
+    context['links'] = links
     return context
