6713bebd74ee577244c876f01f73553ddb895f64
zero_loss <elmq0022@umn.edu>
2017-05-12 23:32:02 -0500

    adding gravitar

--- hanyu/templatetags/AdminLTE.py
+++ hanyu/templatetags/AdminLTE.py
@@ -1,5 +1,9 @@
-from django import template 
+from hashlib import md5
+
+from django import template
 from django.core.urlresolvers import reverse
+from django.utils.html import format_html
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 register = template.Library()
 
@@ -18,3 +22,16 @@ def menu_tree(context, heading, *args):
     context['heading'] = heading
     context['links'] = links
     return context
+
+
+@register.simple_tag(takes_context=True)
+def gravitar(context, **kwargs):
+    html = r'<img src="https://www.gravatar.com/avatar/{}?d={}"?s={}/ class="{}" alt="User Image">'
+    image_id = '00000000000000000000000000000000'
+    image_size = kwargs.get('size', 20)
+    image_default = kwargs.get('default', '')
+    image_class = kwargs.get('class', 'img-circle')
+    user = context['user']
+    if user.is_authenticated:
+        image_id = md5(user.email.encode('utf-8')).hexdigest()
+    return format_html(html, image_id, image_default, image_size, image_class)
