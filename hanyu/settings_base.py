5f7383c76641f445ea15c4f2b6f980e91b2f8972
zero_loss <elmq0022@umn.edu>
2017-05-14 11:34:13 -0500

    fixed the static files dirs

--- hanyu/settings_base.py
+++ hanyu/settings_base.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
 
 
 # Files Paths for the Stanford Segmenter. This is a java library accesed via
