cb383836e52e0db53f24d4d2ef517ede597e9fe2
zero_loss <elmq0022@umn.edu>
2017-05-19 08:45:43 -0500

    created a quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,34 @@
+'''
+Models for the quiz application.
+'''
+
+from django.contrib.auth.admin import User
 from django.db import models
 
-# Create your models here.
+from dictionary.models import Entry
+
+
+class Quiz(models.Model):
+    '''
+    This model represents a single quiz question.
+    The response will be recorded as no response, incorrect, or correct.
+    Results will be stored for the purpose of analysis.
+    Both the questions and answers will be represented by foreign keys
+    to a dictionary.Entry.
+    '''
+    user = models.ForeignKey(User)
+    uid = models.UUIDField(primary_key=True)
+    question = models.ForeignKey(Entry)
+    answer = models.ForeignKey(Entry)
+    date = models.DateTimeField()
+
+    NO_RESPONSE = 'N'
+    INCORRECT = 'I'
+    CORRECT = 'C'
+    RESPONSE = (
+        (NO_RESPONSE, 'no response'),
+        (INCORRECT, 'incorrect'),
+        (CORRECT, 'correct'),
+    )
+
+    response = models.CharField(max_length=1, choices=RESPONSE, default=NO_RESPONSE)
