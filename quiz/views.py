b616ec6941b794d09a830b06b4fc29587f791f05
zero_loss <elmq0022@umn.edu>
2017-05-20 11:56:05 -0500

    creating an initial view

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from random import randint
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from .models import Quiz
+
+
+class QuizView(TemplateView):
+    template_name = 'quiz/quiz.html'
+
+    def get_random_quiz(self):
+        count = Quiz.objects.all().count()
+        return count
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['count'] = self.get_random_quiz()
+        return context
+
