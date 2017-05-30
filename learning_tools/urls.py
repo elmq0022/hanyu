0b63207774be706c63e1344b38c755f19f2a0e20
zero_loss <elmq0022@umn.edu>
2017-05-30 12:48:27 -0500

    fixing the links for the site

--- learning_tools/urls.py
+++ learning_tools/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'words/(?P<status>UN|AC|LN)/', views.WordStatusView.as_view(), name='word_status'),
+    url(r'words/(?P<status>UN|AC|LN)/$', views.WordStatusView.as_view(), name='word_status'),
     url(r'update_word_status/$', views.WordStatusUpdateView.as_view(), name='update_word_status'),
 ]
